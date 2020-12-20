3bb70bd2eaa5e0a332efbfc938a35e1e7752f100
pebl <pawel_smusz@yahoo.com>
2020-12-20 18:51:13 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = 'dbde4b1607554e11a90a4eac514cf7f5.vfs.cloud9.eu-central-1.amazonaws.com'
+  
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
